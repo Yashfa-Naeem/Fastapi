35f5e0894acb35f8a753ca90155205813790c2d4
Yashfa Naeem <yashfanaeem22@gmail.com>
2026-01-28 17:43:44 +0500

    added properties

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app=FastAPI()
+class Item(BaseModel):
+    id : int
+    detail : bool = False
+@app.get("/items/{item_id}")
 
+
+
+    
 
