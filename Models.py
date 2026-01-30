a3bd1b577161c71be4645dd6f92552fff2efdea7
Yashfa Naeem <yashfanaeem22@gmail.com>
2026-01-30 13:26:54 +0500

    got products from specific id

--- Models.py
+++ Models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-class Product(BaseModel):
+class Product(BaseModel):   #we don't need constructor when BaseModel is used
     id : int
     name : str
     description : str
--- main.py
+++ main.py
@@ -10,12 +10,20 @@ def greet():
 products =[
     Product(id=1,name="laptop",description="budget phone",price=100,quantity=2),
     Product(id=2,name="bag",description="budget phone",price=99,quantity=6),
-    Product(id=3,name="phone",description="budget phone",price=200,quantity=3)
+    Product(id=3,name="phone",description="budget phone",price=200,quantity=3),
 ]
 
 @app.get("/products")
-def get_all_products():
+def get_all_products():                               #python -m uvicorn main:app --reload
     return products
+
+@app.get("/product/{id}")
+def get_one_product(id:int):
+     for product in products:
+          if product.id==id:   #if product id matches that of URL
+            return product
+     else:
+          return "product not found"
 
 
 
