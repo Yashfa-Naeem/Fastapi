6889e2c2ea744d610f57a8fbc6e0bc855834f2ac
Yashfa Naeem <yashfanaeem22@gmail.com>
2026-01-30 11:27:08 +0500

    greet

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-app=FastAPI()
-class Item(BaseModel):
-    id : int
-    detail : bool = False
-@app.get("/items/{item_id}")
+
+
 
 
 
