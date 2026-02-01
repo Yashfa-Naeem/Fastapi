dd0e002e2cc0cb35cecd717f3176b6c653283254
Yashfa Naeem <yashfanaeem22@gmail.com>
2026-02-01 21:17:59 +0500

    added new changes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ products =[
     Product(id=3,name="phone",description="budget phone",price=200,quantity=3),
 ]
 
-@app.get("/products")
+@app.get("/product")
 def get_all_products():                               #python -m uvicorn main:app --reload
     return products
 
@@ -23,10 +23,27 @@ def get_one_product(id:int):
           if product.id==id:   #if product id matches that of URL
             return product
      else:
-          return "product not found"
+          return "product not found" 
 
+@app.post("/product")
+def add_product(product:Product):  #variable name and type hint,input expected
+    products.append(product)
+    return product
 
+@app.put("/product")
+def udpate_product(id:int,product:Product):
+    for i in range(len(products)):
+        if products[i].id==id:
+         products[i]=product
+         return "product added successfully"
+    return "no product found"
 
-
+@app.delete("/product")
+def delete_product(id:int):
+    for i in range(len(products)):
+        if products[i].id==id:
+         del products[i]
+         return "product deleted successfully"
+    return "no product found"
     
 
